8a016e5301a32f996924472234fc166d254e29b0
trasqual <tolgaakad@gmail.com>
2023-12-25 21:52:53 +0300

    opened class for mergefinder

---
+++ Assets/Scripts/MergeSystem/MergeFinder.cs
@@ -0,0 +1,13 @@
+using Gameplay.GridSystem;
+using UnityEngine;
+
+namespace Gameplay.MergeableSystem
+{
+    public class MergeFinder : MonoBehaviour
+    {
+        public static void FindMerge(GridCell cell)
+        {
+
+        }
+    }
+}
